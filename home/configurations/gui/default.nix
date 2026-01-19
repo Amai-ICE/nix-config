aaff6dc1b3d8cda451d04da5f7c8e89354c0c6f2
Amai-ICE <138089174+Amai-ICE@users.noreply.github.com>
2026-01-20 04:09:54 +0900

    add: kitty & libreoffice

--- home/configurations/gui/default.nix
+++ home/configurations/gui/default.nix
@@ -3,9 +3,11 @@
     ./alacritty
     ./game
     ./kde
+    ./kitty.nix
     ./jetbrains.nix
     ./editor
     ./obs.nix
     ./entangled.nix
+    ./libreoffice.nix
   ];
 }
---
+++ home/configurations/gui/kitty.nix
@@ -0,0 +1,13 @@
+{
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "JetBrains Mono Nerd";
+    };
+    shellIntegration.enableFishIntegration = true;
+    settings = {
+      background_opacity = 0.65;
+      dynamic_background_opacity = "yes";
+    };
+  };
+}
