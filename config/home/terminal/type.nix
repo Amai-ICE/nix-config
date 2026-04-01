64c726de503280d68675f855eb282e48f6984bfe
Amai-ICE <138089174+Amai-ICE@users.noreply.github.com>
2026-04-01 17:06:59 +0900

    remove: 移動した設定の消去

--- config/home/terminal/type.nix
+++ config/home/terminal/type.nix
@@ -10,12 +10,4 @@
       description = "Terminal emulator type";
     };
   };
-  options.my.home.terminal.kitty = {
-    enable = lib.mkEnableOption "Enable kitty terminal";
-    fontSize = lib.mkOption {
-      type = lib.types.int;
-      default = 12;
-      description = "Kitty font size";
-    };
-  };
 }
