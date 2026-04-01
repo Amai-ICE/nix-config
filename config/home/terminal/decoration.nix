ae3d62aa6beca9ef9299daf23ec1617eb253d011
Amai-ICE <138089174+Amai-ICE@users.noreply.github.com>
2026-04-01 15:23:27 +0900

    remove: 使ってない設定を消去

--- config/home/terminal/decoration.nix
+++ config/home/terminal/decoration.nix
@@ -3,8 +3,6 @@
   options.my.home.terminal.decoration = {
     enable = lib.mkEnableOption "Enable terminal decorations";
     fish.enable = lib.mkEnableOption "Enable fish shell";
-    fastfetch.enable = lib.mkEnableOption "Enable fastfetch";
     starship.enable = lib.mkEnableOption "Enable starship";
-    direnv.enable = lib.mkEnableOption "Enable direnv integration";
   };
 }
